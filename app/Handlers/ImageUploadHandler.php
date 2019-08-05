8e39f90fbb425adc9d5248db5aca32aaafa65113
Leaves <1628043770@qq.com>
2019-08-05 08:43:56 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -3,10 +3,11 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler{
     protected $allowed_ext = ['png','jpg','gif','jpeg'];
-    public function save($file,$folder,$file_prefix){
+    public function save($file,$folder,$file_prefix,$max_width=false){
         $folder_name = "uploads/images/$folder/".date("Ym/d",time());
         $upload_path = public_path().'/'.$folder_name;
         $extension = strtolower($file->getClientOriginalExtension())?: 'png';
@@ -15,9 +16,20 @@ class ImageUploadHandler{
             return false;
         }
         $file->move($upload_path,$filename);
+        if($max_width && $extension != 'gif'){
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
         return [
             'path'=>config('app.url')."/$folder_name/$filename"
         ];
+    }
+    public function reduceSize($file_path,$max_width){
+        $image = Image::make($file_path);
+        $image->resize($max_width,null,function($constraint){
+            $constraint->aspectRatio();//设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->upsize();//防止裁图时图片尺寸变大
+        });
+        $image->save();
     }
 
 }
