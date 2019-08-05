de915f3c57a031fcd57bc51e5ac2db5e2fdcbe9e
Leaves <1628043770@qq.com>
2019-08-05 06:49:14 +0000

    用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    public function update(Request $request){
+
+    }
+}
