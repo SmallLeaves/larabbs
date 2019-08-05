831fd2ec9cf5c625ece51648367c85979eb2ab92
Leaves <1628043770@qq.com>
2019-08-05 07:21:01 +0000

    更新用户信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,16 @@ class UsersController extends Controller
     public function edit(User $user){
         return view('users.edit',compact('user'));
     }
-    public function update(Request $request){
-
+    public function update(Request $request,User $user){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'introduction' => 'nullable|max:140'
+        ]);
+        $user->update([
+            'name' => $request->name,
+            'introduction' => $request->introduction,
+        ]);
+        session()->flash('success','更新成功');
+        return back();
     }
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title',$user->name)
+@section('content')
+<div class="container">
+  <div class="col-md-8 offset-md-2">
+
+    <div class="card">
+      <div class="card-header">
+        <h4><i class="glyphicon glyphicon-edit">编辑个人资料</i></h4>
+      </div>
+
+      <div class="card-body">
+
+        <form action="{{route('users.update',$user->id)}}" method="POST" accept-charset="UTF-8">
+
+          <input type="hidden" name="_method" value="PUT">
+          {{csrf_field()}}
+
+          <div class="form-group">
+            <label for="name-field">用户名</label>
+            <input type="text" name="name" id="name-field" class="form-control" value="{{old('name',$user->name)}}">
+          </div>
+
+          <div class="form-group">
+            <label for="email-field">邮箱</label>
+            <input type="text" class="form-control" name="email" id="email-field" value="{{old('email',$user->email)}}" disabled>
+          </div>
+
+          <div class="form-group">
+            <label for="introduction-field">个人简介</label>
+            <input type="text" class="form-control" name="introduction" id="introduction-field" value="{{old('introduction',$user->introduction)}}">
+          </div>
+
+          <div class="well well-sm">
+            <button class="btn btn-primary" type="submit">保存</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+
+  </div>
+</div>
+@stop
